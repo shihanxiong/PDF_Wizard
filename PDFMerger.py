14eec980aa14c60d50215e736ab1df26dc86204e
Hanxiong Shi <hanxiong@havenlife.com>
2022-02-06 09:38:25 -0500

    feat: added feature to support jpeg conversion

--- PDFMerger.py
+++ PDFMerger.py
@@ -1,16 +1,27 @@
 # Loading the pyPdf Library
 from PyPDF2 import PdfFileMerger
+from PIL import Image
 import os
 
 files = os.listdir('input')
 
-print files
+print(files)
 
 merger = PdfFileMerger()
 
+# convert all `.jpeg` files to PDF
+for file in files:
+    if file.endswith('.jpeg'):
+        image = Image.open('input/' + file)
+        im = image.convert('RGB')
+        im.save(r'input/' + file.replace('.jpeg', '') + '.pdf')
+
+files = os.listdir('input')
+
+# combine all PDF files
 for file in files:
     if file.endswith(".pdf"):
         merger.append(open('input/' + file, 'rb'))
 
-with open('test_1_materials.pdf', 'wb') as fout:
+with open('output/output.pdf', 'wb') as fout:
     merger.write(fout)
