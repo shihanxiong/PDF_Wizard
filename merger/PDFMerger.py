569b8d1851d37eba62ce1ee267a65658e306ff35
Hanxiong Shi <shihanxiong@gmail.com>
2022-09-20 00:06:53 -0400

    feat: re-arranged merger function files

--- merger/PDFMerger.py
+++ merger/PDFMerger.py
@@ -3,7 +3,9 @@ from PyPDF2 import PdfFileMerger
 from PIL import Image
 import os
 
-files = os.listdir('input')
+input_dir = 'merger/input/'
+output_dir = 'merger/output/'
+files = os.listdir(input_dir)
 
 print(files)
 
@@ -12,16 +14,16 @@ merger = PdfFileMerger()
 # convert all `.jpeg` files to PDF
 for file in files:
     if file.endswith('.jpeg'):
-        image = Image.open('input/' + file)
+        image = Image.open(input_dir + file)
         im = image.convert('RGB')
         im.save(r'input/' + file.replace('.jpeg', '') + '.pdf')
 
-files = os.listdir('input')
+files = os.listdir(input_dir)
 
 # combine all PDF files
 for file in files:
     if file.endswith(".pdf"):
-        merger.append(open('input/' + file, 'rb'))
+        merger.append(open(input_dir + file, 'rb'))
 
-with open('output/output.pdf', 'wb') as fout:
+with open(output_dir + 'output.pdf', 'wb') as fout:
     merger.write(fout)
