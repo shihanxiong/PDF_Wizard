e2c74ca96e355307fb0ffba6a20fff93ee28741f
Hanxiong Shi <hanxiong@havenlife.com>
2024-04-06 17:35:42 -0400

    fix: fixed deprecated functions in splitter script

--- splitter/PDFSplitter.py
+++ splitter/PDFSplitter.py
@@ -1,21 +1,21 @@
-from PyPDF2 import PdfFileReader, PdfFileWriter
+from PyPDF2 import PdfReader, PdfWriter
 import os
 
-input_dir = 'spliter/input/'
-output_dir = 'spliter/output/'
-input_file = input_dir + 'input.pdf'
+input_dir = 'splitter/input/'
+output_dir = 'splitter/output/'
+input_file = input_dir + '2023_TaxReturn.pdf'
 output_file = output_dir + 'output.pdf'
 
-reader = PdfFileReader(open(input_file, 'rb'))
-writer = PdfFileWriter()
+reader = PdfReader(open(input_file, 'rb'))
+writer = PdfWriter()
 
-start = 52
-end = 79
+start = 41
+end = 44
 
 start -= 1
 with open(output_file, "wb") as f:
     for i in range(start, end):
-        writer.addPage(reader.getPage(i))
+        writer.add_page(reader.pages[i])
         writer.write(f)
         i += 1
     print("PDF splitting process complete")
